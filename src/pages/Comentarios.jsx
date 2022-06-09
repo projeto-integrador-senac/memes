e149196e640fe2c5dccff8a25e94cff8eaf3b8ff
hudson-correa <105455805+hudson-correa@users.noreply.github.com>
2022-06-09 16:46:27 -0300

    Alteração

--- src/pages/Comentarios.jsx
+++ src/pages/Comentarios.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import styles from '../styles/Comentarios.module.css'
+import styles from '../styles/Comentarios.module.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faArrowLeft} from "@fortawesome/free-solid-svg-icons"
+import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
 
 const Comentarios = () => {
     return ( 
     <div className={styles.MenuFeed}>
-        <div>
+     <button className={styles.botaoVoltar}> <FontAwesomeIcon icon={faArrowLeft}/> </button>
+       <hr></hr>
+        <div className={styles.DescricaodoUser} >
+            <div className={styles.NmdouserEarroba}>
+                <p >Nome do user</p>
+                <p className={styles.arroba}>@user</p>
+            </div>
             <div>
-                <button> <FontAwesomeIcon icon={faArrowLeft}/> </button>
-                <div>
-                    <img src='https://pbs.twimg.com/profile_images/1532112034871099392/QGRSRfSh_400x400.jpg' className={styles.FotoCapa1}/>
+                <div> 
+                    <img src='https://pbs.twimg.com/profile_images/1532112034871099392/QGRSRfSh_400x400.jpg' className={styles.FtoUsuario}/>
                 </div>
-                <p>Nome do user</p>
-            </div> 
+            </div>
+        </div>
+
+        <div className={styles.Postagem}>  
             <p>comentário do proprietário </p>
+            <img src='https://i.em.com.br/RWp3rirEmujT4if9HsjRMJfj1d0=/790x/smart/imgsapp.em.com.br/app/noticia_127983242361/2022/02/27/1348761/putin-alimenta-cachorro-com-a-cara-de-bolsonaro_1_38500.jpg' className={styles.Imgpostada}/>
         </div>
-        <div>
-            <img src='https://i.em.com.br/RWp3rirEmujT4if9HsjRMJfj1d0=/790x/smart/imgsapp.em.com.br/app/noticia_127983242361/2022/02/27/1348761/putin-alimenta-cachorro-com-a-cara-de-bolsonaro_1_38500.jpg' className={styles.FotoMeme1} />
-        </div>
+
     </div>
     );
 }

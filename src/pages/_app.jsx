a9c1427b99d9065c12c242499cc8c4bb5609f45d
MuriloPlayerOne <74666594+MuriloPlayerOne@users.noreply.github.com>
2022-06-10 15:01:11 -0300

    update react

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import '../styles/globals.css'
 import Head from "next/head";
 import "@fortawesome/fontawesome-svg-core/styles.css";
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Menu from "../components/menu";
 import styles from '../styles/Index.module.css';
 import Link from 'next/dist/client/link';

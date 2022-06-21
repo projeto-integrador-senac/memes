f64d9677d97e1328b5df221e9f47cf123db74eac
MuriloPlayerOne <74666594+MuriloPlayerOne@users.noreply.github.com>
2022-06-21 17:02:23 -0300

    atualizar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -38,12 +38,44 @@ const Inicio = () => {
 </span>
   </article>
   <article className={styles.browser}>
-    <h2>Mozilla Firefox</h2>
-    <p>Mozilla Firefox is an open-source web browser developed by Mozilla. Firefox has been the second most popular web browser since January, 2018.</p>
+  <div className={styles.imagem}>
+  <img src="https://pbs.twimg.com/profile_images/1532112034871099392/QGRSRfSh_400x400.jpg"/>
+  </div>
+  <Link href="/usuario">
+    <h2>Paysandu<FontAwesomeIcon className={styles.checkbox} icon={faCheckCircle}/></h2></Link>
+    <p className={styles.pp}>@Paysandu</p>
+    <Link href="/comentarios">
+    <div>
+      <img className={styles.post} src="https://pbs.twimg.com/media/FUls1VgXoAAlpE5?format=jpg&name=small" />
+    </div></Link>
+    <FontAwesomeIcon icon={faHeart} className={styles.heart}/>
+    <Link href="/comentarios">
+    <FontAwesomeIcon icon={faComment} className={styles.comment}/></Link>
+    <FontAwesomeIcon icon={faBookmark} className={styles.bookmark}/>
+    <span className={styles.twheartbox}>
+       <input className={styles.checktool} type="checkbox" name="name" value=""/>
+    <span className={styles.twheart}></span>
+</span>
   </article>
   <article className={styles.browser}>
-    <h2>Microsoft Edge</h2>
-    <p>Microsoft Edge is a web browser developed by Microsoft, released in 2015. Microsoft Edge replaced Internet Explorer.</p>
+  <div className={styles.imagem}>
+  <img src="https://pbs.twimg.com/profile_images/1532112034871099392/QGRSRfSh_400x400.jpg"/>
+  </div>
+  <Link href="/usuario">
+    <h2>Paysandu<FontAwesomeIcon className={styles.checkbox} icon={faCheckCircle}/></h2></Link>
+    <p className={styles.pp}>@Paysandu</p>
+    <Link href="/comentarios">
+    <div>
+      <img className={styles.post} src="https://pbs.twimg.com/media/FUls1VgXoAAlpE5?format=jpg&name=small" />
+    </div></Link>
+    <FontAwesomeIcon icon={faHeart} className={styles.heart}/>
+    <Link href="/comentarios">
+    <FontAwesomeIcon icon={faComment} className={styles.comment}/></Link>
+    <FontAwesomeIcon icon={faBookmark} className={styles.bookmark}/>
+    <span className={styles.twheartbox}>
+       <input className={styles.checktool} type="checkbox" name="name" value=""/>
+    <span className={styles.twheart}></span>
+</span>
   </article>
 </article>
 

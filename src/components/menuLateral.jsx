9a9015d0208703d893de75fbaa53822e063167e9
hudson-correa <105455805+hudson-correa@users.noreply.github.com>
2022-06-30 15:44:55 -0300

    hkasggd

--- src/components/menuLateral.jsx
+++ src/components/menuLateral.jsx
@@ -4,8 +4,24 @@ import Link from 'next/dist/client/link';
 import { home, explore, bookmarks, lists, profile, more, messages } from '../pages/api/icons_teste'
 import styles from '../styles/MenuLateral.module.scss'
 import Postagem from './postagem';
+import Swal from 'sweetalert2';
+import  Router  from 'next/dist/client/router';
 
 const MenuLateral = (props) => {
+
+  //props useStates no index para mostrar postagem
+
+  const [iduser, alteraIduser] = React.useState(null)
+    React.useEffect(() => {
+    alteraIduser(localStorage.getItem("iduser"))
+  }, [])
+
+  const Postagem = () => {
+
+    //document.querySelector(".modal").style.display = "block"
+
+  }
+
   return (
     <div className={styles.leftpane}>
       <div className={styles.container}>
@@ -17,31 +33,42 @@ const MenuLateral = (props) => {
             <span>{home} Inicio</span>
             </button>
           </Link>
-          <Link href="/galeria" activeClassName={styles.selected}>
-          <button className="more">
-            <span>{bookmarks} Galeria</span>
-            </button>
-          </Link>
+
           <Link href="/museu" activeClassName={styles.selected}>
           <button className="more">
             <span>{lists} História dos Memes</span>
           </button>
           </Link>
-          <Link href="/perfil" activeClassName={styles.selected}>
-          <button className="more">
-            <span>{profile} Perfil</span>
-        </button>
-          </Link>
+          
           <Link href="/criadores" activeClassName={styles.selected}>
           <button className="more">
             <span>{more} Criadores</span>
           </button>
           </Link>
+
+
+
+          <button onClick={() => iduser ? Router.push('/galeria') : Swal.fire({text: "crie uma conta ou faça login para continuar.", footer: "<a href='/login'> Já possui uma conta? </a>"}) } >
+            <span>{bookmarks} Galeria</span>
+          </button>
+        
+          
+         
+          <button onClick={() => iduser ? Router.push('/perfil') : Swal.fire({text: "crie uma conta ou faça login para continuar.", footer: "<a href='/login'> Já possui uma conta? </a>"}) }>
+            <span>{profile} Perfil</span>
+        </button>
+          
+          
+         
+         
         </nav>
         
         
-
-        <button onClick={Postagem} className={styles.tweet}>Postar</button>
+      {iduser ?
+        <button  onClick={Postagem} className={styles.tweet}>Postar</button>
+      :
+      <button className={styles.tweet} onClick={ () => Swal.fire({text: "crie uma conta ou faça login para continuar.", footer: "<a href='/login'> Já possui uma conta? </a>"})}  >Postar</button>
+      }
         
 
         <Postagem/>
--- src/components/postagem.jsx
+++ src/components/postagem.jsx
@@ -5,85 +5,9 @@ import Swal from 'sweetalert2';
 const Postagem = () => {
 
 
-var not = [];
-var nome = [];
 
-if (typeof window !== "undefined") {
-    // browser code
-  
+    if (typeof window !== "undefined") {
 
-
-function cadasNot(){
-
-   
-    var not_digi;
-    not_digi = document.getElementById("inputNot").value;
-
-   
-    var nome_digi;
-    nome_digi = document.getElementById("inputNome").value;
-
-   
-    if(not_digi.trim() == "" || nome_digi.trim() == ""){
-        Swal.fire({
-            icon: 'error',
-            title: 'OPA.. ERRO',
-            text: 'SEM NADA NÃO DÁ PRA POSTAR NÉ',
-          })
-        return
-    }
-
-
-    Swal.fire({
-        icon: 'success',
-        title: 'Boaa ',
-        text: 'Noticia Postada amigão',
-      })
-
-    
-    not.push(not_digi);
-    nome.push(nome_digi);
-
-    
-    var html = "";
-    html += "<ul>";
-    for( var i = 0; i < not.length; i++ ){
-        html += "<li> <strong>" + nome[i] + "</strong> <br> " + not[i] + "</li> <br>";
-    }
-    html += "</ul>";
-
-    
-    document.getElementById("listaNot").innerHTML = html;
-
-    
-    document.getElementById("listaEscritor").innerHTML = nome;
-
-    
-    escondeModall();
-}
-
-function mostraNoti(){
-    document.getElementsByClassName("modal")[0].style.display = "block"
-    document.getElementsByClassName("fundo")[0].style.display = "block"   
-
-    document.getElementById('inputNome').value = '';
-
-    document.getElementById('inputNot').value = '';
-
-    document.getElementById('inputNome').focus();
-}
-
-function escondeModall(){
-    
-    document.getElementsByClassName("modal")[0].style.display = "none"
-    document.getElementsByClassName("fundo")[0].style.display = "none"
-}
-
-
-    function escondeModal(){
-        document.getElementsByClassName("modal")[0].style.display = "none"
-        document.getElementsByClassName("fundo")[0].style.display = "none"
-}
 
     const axios = require('axios').default;
 
@@ -92,8 +16,8 @@ function escondeModall(){
 
         p.preventDefault()
 
-        const descricao = document.getElementById("inptDescricao").value;
-        const postagem = document.getElementById("inputPostagem").value;
+        const descricao = document.getElementById("inputDescricao").value;
+        const postagem = document.getElementById("inputMeme").value;
         
         const obj = {
             descricao : descricao,
@@ -130,28 +54,35 @@ function escondeModall(){
         })
     }
         
+    }
+
+ const escondeModal = () => {
+
+    console.log("blablabla")
+        //document.getElementById("inputSair").none;
+
+    }
 
 
 
+        return ( 
+            <>
+    <div class={styles.modal}>
+        Poste Um Meme
+        <form onSubmit={(p) => cadastroPostagem(p)}>
+            <input id="inputDescricao" placeholder="Descrição" />
 
-    return ( 
-        <>
-<div class={styles.modal}>
-    Poste Um Meme
-    <form onSubmit={(p) => cadastroPostagem(p)}>
-        <input id="inputDescricao" placeholder="Descrição" />
-
-        <input id="inputMeme" placeholder="URL do MEME" />
-    
-    </form>
-    <div >
-        <button className={styles.salvar} onclick="abacaxi();"  >SALVAR</button>   
-        <button className={styles.cancela} onclick={escondeModal}>SAIR</button>
+            <input id="inputMeme" placeholder="URL do MEME" />
+        
+        </form>
+        <div >
+            <button className={styles.salvar} onclick="abacaxi();"  >SALVAR</button>   
+            <button className={styles.cancela} id="inputSair" onclick= {() => escondeModal()} >SAIR</button>
+        </div>
     </div>
-</div>
-        </>
-     );
+            </>
+        );
 }
-}
+
  
 export default Postagem;

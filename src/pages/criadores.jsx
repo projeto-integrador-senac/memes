064a316e358fa04ebc1454a4ef44e4fc6ae8563d
hudson-correa <105455805+hudson-correa@users.noreply.github.com>
2022-06-30 22:37:31 -0300

    altera

--- src/pages/criadores.jsx
+++ src/pages/criadores.jsx
@@ -13,8 +13,6 @@ const Criadores = () => {
         <Postagem/>
                 <div className={styles.text}>
             <h1 style={{color: '#157e8f', fontSize: '50px'} }>CRIADORES</h1>
-
-            <p>Saiba tudo sobre os criadores desse site</p>
             </div>   
 
             <hr width="80%"  className={styles.hr}/>
@@ -42,17 +40,7 @@ const Criadores = () => {
                 <div id="listaNot"  className={styles.listaNot}> </div>
         </div>
         </div>
-   <div  className={styles.not}>
-            <h3 className={styles.lista} style={{fontSize: "large"}}> Referência </h3>
-            <ul className={styles.lista}>
-                <li>DUONG, Iris. Why do memes matter?: a look at the good, the dank, and the viral. 2017. Disponível em: https://www.scmp.com/yp/discover/lifestyle/features/article/3065773/why-do-memes-matter-look-good-dank-and-viral. Acesso em: 10 jun. 2022. </li>
-                <li>FULTON, Wil. I Found the World's First Meme With Help From Meme Historians. 2017. Disponível em: https://www.thrillist.com/entertainment/nation/first-meme-ever. Acesso em: 09 jun. 2022. </li>
-                <li>MARCUS. What Are Memes Really?. TEDx Talks. 2017. Disponível em: https://www.youtube.com/watch?v=9hU4mzq0d1Q. Acesso em: 08 jun. 2022. </li>
-                <li>Rogers, Kara. "meme". Encyclopedia Britannica. 2021. Disponível em: https://www.britannica.com/topic/meme. Acesso 10 jun. 2022. </li>
-                <li>THE Original Memes. 2018. Disponível em: https://www.dictionary.com/e/the-original-memes-before-memes-listicle/. Acesso em: 09 jun. 2022.</li>
-            </ul>
-             <div id="listaNot"  className={styles.listaNot}> </div>
-        </div>
+  
         <div  className={styles.rodape}>
         <div  className={styles.escreva}>
             <h3>© 2022 Meme's Gallerry, Inc.</h3>
